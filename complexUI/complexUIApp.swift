fadcdc59bf61693ef0d6aa347af6bd8dd7bbdbe0
hazem-smawy <hazemsmawy@gmail.com>
2022-10-11 11:03:10 +0300

    Initial Commit

---
+++ complexUI/complexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  complexUIApp.swift
+//  complexUI
+//
+//  Created by hazem smawy on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct complexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
